5f088564591791314416db1ea0fa6749a4061a29
Regina Karkaeva <regina.karkaeva@t-systems.com>
2020-05-17 18:41:40 +0300

    Add method description

--- src/main/java/com/myit/util/streams/Streams.java
+++ src/main/java/com/myit/util/streams/Streams.java
@@ -8,6 +8,13 @@ import java.util.stream.Collectors;
  */
 public class Streams {
 
+    /**
+     * Excludes the pointed strings from the input list of strings.
+     *
+     * @param sourceStrings the input list of strings
+     * @param excludingStrings the list of strings to be excluded
+     * @return the list of strings which doesn't contain excluding strings
+     */
     List<String> exclude(List<String> sourceStrings, List<String> excludingStrings) {
         List<String> resultingStrings = sourceStrings.stream()
                 .filter(str -> excludingStrings.stream().noneMatch(exclStr -> str.startsWith(exclStr.toUpperCase())))
